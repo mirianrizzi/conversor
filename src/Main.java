4b2100e9cf5b3ee4b43d3b8bec90c93e5b9b6abb
mirianrizzi <90937750+mirianrizzi@users.noreply.github.com>
2021-12-02 18:30:50 -0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,6 @@ public class Main {
         final float Reaumur = c * 0.8f;
         final float Rankine = (c * 1.8f) + 491;
 
-        System.out.println("As temperaturas convertidas são:");
         System.out.printf("A temperatura em Fahrenheit é %.2f %n", Fahrenheit);
         System.out.printf("A temperatura em Kelvin é %.2f %n", Kelvin);
         System.out.printf("A temperatura em Reaumur é %.2f %n", Reaumur);
